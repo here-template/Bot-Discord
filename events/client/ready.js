11191fa717c82c27d18d0c5829939ef951442c94
Cleboost <clement.balarot@gmail.com>
2023-04-22 19:39:44 +0000

    maj api

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,11 +1,11 @@
 const client = require("../../index").client;
 const clc = require("cli-color");
-const { ActivityType } = require("discord.js");
+const { ActivityType, Status } = require("discord.js");
 
 client.on("ready", async () => {
 	client.user.setPresence({
-		activities: [{ name: "la communauté", type: ActivityType.Watching }],
-		status: "online",
+		activities: [{ name: "status personnalisé", type: ActivityType.Watching }],
+		status: Status.Connecting,
 	});
 	
 	await client.application.commands.set(client.commands.map((cmd) => cmd));
