449c62d490e552743958f2770751b52e4d395ee5
Youritch <86657843+Youritch@users.noreply.github.com>
2023-07-04 10:28:19 +0200

    test "userOnly" button

--- interactions/commands/exHandler/sendButton.js
+++ interactions/commands/exHandler/sendButton.js
@@ -1,21 +1,22 @@
-const { CommandInteraction, Client, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require("discord.js");
+const {CommandInteraction, Client, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require("discord.js");
 
 module.exports = {
-  name: "sendbutton",
-  description: "Envoie un bouton de test",
-  /**
-   * @param {ChatInputCommandInteraction} interaction
-   * @param {Client} client
-   */
-  runInteraction: async (client, interaction) => {
-    const row = new ActionRowBuilder()
-      .addComponents(
-        new ButtonBuilder()
-        .setCustomId("sendbutton") //Custom ID du bouton pour interactions/buttons/sendButton.js
-        .setLabel("Test")
-        .setStyle(ButtonStyle.Primary) //Style du bouton avoir sur https://discordjs.guide/message-components/buttons.html#button-styles
-      )
-
-    interaction.reply({ content: "Voila le bouton de test !", components: [row] });
-  },
+	name: "sendbutton",
+	description: "Envoie un bouton de test",
+	userOnly: true,
+	/**
+	 * @param {ChatInputCommandInteraction} interaction
+	 * @param {Client} client
+	 */
+	runInteraction: async (client, interaction) => {
+		const row = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId("sendbutton") //Custom ID du bouton pour interactions/buttons/sendButton.js
+					.setLabel("Test")
+					.setStyle(ButtonStyle.Primary) //Style du bouton avoir sur https://discordjs.guide/message-components/buttons.html#button-styles
+			);
+		
+		interaction.reply({content: "Voila le bouton de test !", components: [row]});
+	},
 };
