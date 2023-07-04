a51e415e31b21e9d4adc33f045662b873827f2d3
Youritch <86657843+Youritch@users.noreply.github.com>
2023-07-04 09:37:22 +0200

    suite redev hadler

--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -1,23 +1,26 @@
 const client = require("./../../index.js").client;
-const clc = require("cli-color");
+const {redBright, bold} = require("cli-color");
 const interType = {
 	2: require("../../handlers/interactionValidation/command.js"),
-	3: require("../../handlers/interactionValidation/component.js"),//discord ne différencie pas les btns des selects
+	3: require("../../handlers/interactionValidation/component.js"),//discord ne differencie pas les btns des selects
 	4: require("../../handlers/interactionValidation/autocomplete.js"),
 	5: require("../../handlers/interactionValidation/modal.js")
-}
+};
 client.on("interactionCreate", async (interaction) => {
 	console.log(interaction.type);
 	if (interaction.user.bot) return console.log(`Le bot ${interaction.user.username} a tenté de faire une commande !`);
-	const inter = interType[interaction.type](client,interaction);
+	const inter = interType[interaction.type](client, interaction);
 	console.log(inter);
-	if (!inter[0]) return interaction.reply(inter[1]);
+	if (!inter[0]) return interaction.reply(inter[1] ?? {
+		content: "Cette action ne semble pas exister !",
+		ephemeral: true
+	});
 	try {
 		return await inter[1](client, interaction);
 	} catch (err) {
 		if (!err) return;
-		console.log(clc.redBright.bold(`>> Erreur dans ${interaction.commandName} :`));
+		console.log(redBright.bold(`>> Erreur dans ${interaction.commandName} :`));
 		console.log(err);
-		return interaction.reply({ content: "Il ya une une erreur !", ephemeral: true });
+		return interaction.reply({content: "Une erreur c'est produite !", ephemeral: true});
 	}
 });
