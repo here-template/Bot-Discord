7ab2c031762ef8a2f0418d79975df14b690fe37f
Youritch <86657843+Youritch@users.noreply.github.com>
2023-08-07 17:20:24 +0200

    déplacer cmd exemple (mtn: sans catégorie)

--- interactions/commands/exemple.js
+++ interactions/commands/exemple.js
@@ -4,10 +4,10 @@ module.exports = {
 	name: "exemple",
 	description: "Exemple de structure de commande (ici avec toute les options).",
 	cooldown: 5,//Il faut attendre 5 secondes entre chaque utilisation de cette commande (sauf si elle est noté "devOnly: true")
-	devOnly: true, //Seul les utilisateurs qui ont leur id discord inscrit dans config.js peuvent executer cette commade
-	mpLock: true, //Si true, la commande ne peut être executé que dans un salon d'un serveur et non en mp
-	userPermissions: ["Administrator"], //L'utilisateur a besoin des permissions administrateur pour executer cette commande
-	botPermissions: ["Administrator"], //Le bot a besoin des permissions administrateur pour executer cette commande
+	devOnly: true, //Seuls les utilisateurs qui ont leur id discord inscrit dans config.js peuvent execute cette commande
+	mpLock: true, //Si true, la commande ne peut être execute que dans un salon d'un serveur et non en mp
+	userPermissions: ["Administrator"], //L'utilisateur a besoin des permissions administrateur pour execute cette commande
+	botPermissions: ["Administrator"], //Le bot a besoin des permissions administrateur pour execute cette commande
 	/**
 	 * @param {ChatInputCommandInteraction} interaction
 	 * @param {Client} client
