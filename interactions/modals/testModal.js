cc3e5b7b76650aec0129ffbe5354fa4e1d27ba5e
Youritch <86657843+Youritch@users.noreply.github.com>
2023-08-07 18:26:38 +0200

    correction recup val du modal

--- interactions/modals/testModal.js
+++ interactions/modals/testModal.js
@@ -1,9 +1,14 @@
 module.exports = {
-	customID: "testmodal",
+	customID: "testModal",
+	/**
+	 * @param {ModalSubmitInteraction} interaction
+	 * @param {Client} client
+	 */
 	runInteraction: async (client, interaction) => {
-		const champs1 = interaction.data.values[0];
-		const champs2 = interaction.data.values[1];
-
-		await interaction.reply({content: `Champs 1: ${champs1}\nChamps 2: ${champs2}`, ephemeral: true});
+		console.log(interaction.components);
+		const champs1 = interaction.fields.getTextInputValue("favoriteColorInput");
+		const champs2 = interaction.fields.getTextInputValue("hobbiesInput");
+		
+		await interaction.reply({content: `Champs 1: **${champs1}**\nChamps 2: **${champs2}**`, ephemeral: true});
 	},
 };
