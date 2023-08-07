f95ca24b4fcb8888cd79fde7cbf670b96d7686d1
Youritch <86657843+Youritch@users.noreply.github.com>
2023-08-07 19:32:40 +0200

    debug

--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -6,11 +6,13 @@ const interType = {
 	4: require("../../handlers/interactionValidation/autocomplete.js"),
 	5: require("../../handlers/interactionValidation/modal.js")
 };
+
 client.on("interactionCreate", async (interaction) => {
-	console.log(interaction.type);
+	if (process.env.DEBUG === "true") {
+		console.log(interaction);
+	}
 	if (interaction.user.bot) return console.log(`Le bot ${interaction.user.username} a tenté de faire une commande !`);
 	const inter = interType[interaction.type](client, interaction);
-	console.log(inter);
 	if (!inter[0]) return interaction.reply(inter[1] ?? {
 		content: "Cette action ne semble pas exister !",
 		ephemeral: true
