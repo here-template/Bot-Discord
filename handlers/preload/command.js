e265ec77fbb1e048b2cfc832ddcde4bb3fd50ca9
Youritch <86657843+Youritch@users.noreply.github.com>
2023-08-07 12:38:06 +0200

    petit correction

--- handlers/preload/command.js
+++ handlers/preload/command.js
@@ -1,11 +1,11 @@
-const { yellow } = require("cli-color");
+const {yellow} = require("cli-color");
 const fs = require("fs");
 
 module.exports = (client) => {
 	console.log(yellow.underline("Commandes chargées :"));
 	fs.readdirSync("./interactions/commands/").forEach((dir) => {
 		const files = fs.readdirSync(`./interactions/commands/${dir}/`).filter((file) => file.endsWith(".js"));
-		console.log(yellow.bold("> " + dir + " :"));
+		console.log(yellow.bold(`> ${dir} :`));
 		files.forEach((file) => {
 			let command = require(`../../interactions/commands/${dir}/${file}`);
 			if (command) {
@@ -13,14 +13,16 @@ module.exports = (client) => {
 				if (command.cooldown) {
 					command.cooldown *= 1000;
 					if (command.cooldown > 2147483646) command.cooldown = 2147483646; //ne peut pas dépasser cette valeur
-				};
+				}
+				
 				//si pas de devOnly, par defaut false
 				if (!command.devOnly) command.devOnly = false;
-
+				
 				if (!command.mpLock) {
 					//@Youritch : Besoins de toi pour la suite
-				};
-
+				}
+				
+				
 				//la categorie
 				command.category = dir;
 				//les permissions par défaut :
@@ -32,7 +34,8 @@ module.exports = (client) => {
 				//upload dans le bot
 				client.commands.set(command.name, command);
 				console.log(yellow(`  > ${command.name}`));
-			};
+			}
+			
 		});
 	});
 };
