5e4c4b5969fe2a12d708812ce6390ded1608c90d
Cleboost <clement.balarot@gmail.com>
2023-09-02 09:57:44 +0200

    add verif file and id

--- handlers/preload/command.js
+++ handlers/preload/command.js
@@ -1,17 +1,27 @@
 const {yellow, redBright} = require("cli-color");
 const fs = require("fs");
 
+let listFile = [];
+let listNameCommand = [];
+
 module.exports = (client) => {
 	console.log(yellow.underline("Commandes chargées :"));
 	let dirs = fs.readdirSync("./interactions/commands/").filter((file) => !file.includes("."));
 	dirs.push("../commands");
 	dirs.forEach((dir) => {
+		// Check if the file is not double loaded
 		const files = fs.readdirSync(`./interactions/commands/${dir}/`).filter((file) => file.endsWith(".js"));
 		if (files.length !== 0) console.log(yellow.bold(`> ${dir === "../commands" ? "sans catégorie" : dir} :`));
 		files.forEach((file) => {
+			if (listFile.includes(file)) console.log(redBright.bold(`>> La commande ${file} est déjà chargée ou un deuxième fichier commande à le même nom dans le dossier ${dir} !`));
+			listFile.push(file);
 			let command = require(`../../interactions/commands/${dir}/${file}`);
 			if (command) {
+				// Check if the command name is defined
 				if (command.name === undefined) return console.log(redBright.bold(`>> La commande dans ${file} n'a pas de name !`));
+				// Check if another command don't have the same name
+				if (listNameCommand.includes(command.name)) return console.log(redBright.bold(`>> La commande avec comme nom ${command.name} est déjà chargée ou un doublon existe dans le fichier ${file} situé dans le dossier ${dir} !`));
+				listNameCommand.push(command.name);
 				if (command.description === undefined) return console.log(redBright.bold(`>> La commande dans ${file} n'a pas de description !`));
 				//met le cooldown en ms et ajuste le temps max
 				if (command.cooldown) {
--- handlers/preload/modal.js
+++ handlers/preload/modal.js
@@ -1,14 +1,23 @@
 const fs = require("fs");
 const {black, redBright} = require("cli-color");
 
+const fileList = [];
+const customIDList = [];
+
 module.exports = (client) => {
 	console.log(black.underline("Modals chargés :"));
 	const files = fs.readdirSync(`./interactions/modals/`).filter((file) => file.endsWith(".js"));
 	if (!files || files.length <= 0) ;
 	files.forEach((file) => {
+		if (fileList.includes(file)) console.log(redBright.bold(`>> Le modal ${file} est déjà chargé, ou un deuxième fichier modal à le même nom !`));
+		fileList.push(file);
 		const modal = require(`../../interactions/modals/${file}`);
 		if (modal) {
+			// Check if customID is defined
 			if (modal.customID === undefined) return console.log(redBright.bold(`>> Le modal dans ${file} n'a pas de customID !`));
+			// Check if customID is already used
+			if (customIDList.includes(modal.customID)) return console.log(redBright.bold(`>> Le modal avec comme id ${modal.customID} est déjà chargé ou un doublon existe dans le fichier ${file} !`));
+			customIDList.push(modal.customID);
 			//si pas spécifié alors par defaut false
 			if (!modal.admin) {
 				modal.admin = false;
