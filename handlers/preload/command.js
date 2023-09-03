4a20727f719acd3b63e38bee924e14dd846b7630
Youritch <86657843+Youritch@users.noreply.github.com>
2023-09-03 18:16:34 +0200

    modif protection+desaverif fileName+verif des<100

--- handlers/preload/command.js
+++ handlers/preload/command.js
@@ -1,20 +1,23 @@
 const {yellow, redBright} = require("cli-color");
 const fs = require("fs");
 
-let listFile = [];
-let listNameCommand = [];
+//const listFile = [];
+const listNameCommand = [];
 
 module.exports = (client) => {
+	let dirs = fs.readdirSync("./interactions/commands/");
+	if (dirs.length === 0) return;
 	console.log(yellow.underline("Commandes chargées :"));
-	let dirs = fs.readdirSync("./interactions/commands/").filter((file) => !file.includes("."));
+	dirs = dirs.filter((file) => !file.includes("."));
 	dirs.push("../commands");
 	dirs.forEach((dir) => {
 		// Check if the file is not double loaded
 		const files = fs.readdirSync(`./interactions/commands/${dir}/`).filter((file) => file.endsWith(".js"));
-		if (files.length !== 0) console.log(yellow.bold(`> ${dir === "../commands" ? "sans catégorie" : dir} :`));
+		if (files.length === 0) return;
+		console.log(yellow.bold(`> ${dir === "../commands" ? "sans catégorie" : dir} :`));
 		files.forEach((file) => {
-			if (listFile.includes(file)) console.log(redBright.bold(`>> La commande ${file} est déjà chargée ou un deuxième fichier commande à le même nom dans le dossier ${dir} !`));
-			listFile.push(file);
+			//if (listFile.includes(file)) console.log(redBright.bold(`>> La commande ${file} est déjà chargée ou un deuxième fichier commande à le même nom dans le dossier ${dir} !`));
+			//listFile.push(file);
 			let command = require(`../../interactions/commands/${dir}/${file}`);
 			if (command) {
 				// Check if the command name is defined
@@ -22,7 +25,9 @@ module.exports = (client) => {
 				// Check if another command don't have the same name
 				if (listNameCommand.includes(command.name)) return console.log(redBright.bold(`>> La commande avec comme nom ${command.name} est déjà chargée ou un doublon existe dans le fichier ${file} situé dans le dossier ${dir} !`));
 				listNameCommand.push(command.name);
+				//check description
 				if (command.description === undefined) return console.log(redBright.bold(`>> La commande dans ${file} n'a pas de description !`));
+				if (command.description.length > 100) return console.log(redBright.bold(`>> La commande dans ${file} a une description trop long (${command.description.length}/100) !`));
 				//met le cooldown en ms et ajuste le temps max
 				if (command.cooldown) {
 					command.cooldown *= 1000;
@@ -34,7 +39,6 @@ module.exports = (client) => {
 				//pour bloquer l'utlisation de la commande en mp
 				if (!command.mpLock) command.mpLock = false;
 				
-				
 				//la categorie
 				command.category = dir === "../commands" ? "sans_categorie" : dir.toLowerCase();
 				
@@ -43,7 +47,7 @@ module.exports = (client) => {
 				if (!command.botPermissions) command.botPermissions = [];
 				command.userPermissions.push("SendMessages");
 				command.botPermissions.push("SendMessages");
-				//à faire : vérif taille description ?
+				
 				//upload dans le bot
 				client.commands.set(command.name, command);
 				console.log(yellow(`  > ${command.name}`));
