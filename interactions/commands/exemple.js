ac4008ff669e05cd4fb33e2e1b3a32efd56ed5e6
Youritch <86657843+Youritch@users.noreply.github.com>
2023-11-04 10:02:37 +0100

    maj mp

--- interactions/commands/exemple.js
+++ interactions/commands/exemple.js
@@ -5,7 +5,7 @@ module.exports = {
 	description: "Exemple de structure de commande (ici avec toute les options).",
 	cooldown: 5,//Il faut attendre 5 secondes entre chaque utilisation de cette commande (sauf si elle est noté "devOnly: true")
 	devOnly: true, //Seuls les utilisateurs qui ont leur id discord inscrit dans config.js peuvent execute cette commande
-	mpLock: true, //Si true, la commande ne peut être execute que dans un salon d'un serveur et non en mp
+	mp: true, //Si true, la commande peut être execute en mp, si false elle peut etre executer que sur un serveur
 	userPermissions: ["Administrator"], //L'utilisateur a besoin des permissions administrateur pour execute cette commande
 	botPermissions: ["Administrator"], //Le bot a besoin des permissions administrateur pour execute cette commande
 	/**
