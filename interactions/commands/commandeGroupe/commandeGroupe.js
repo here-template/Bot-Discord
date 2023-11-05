8c4b54853ff710a23c2072ea7b587cdbd259615e
Youritch <86657843+Youritch@users.noreply.github.com>
2023-11-05 10:01:10 +0100

    add comment categori vide

--- interactions/commands/commandeGroupe/commandeGroupe.js
+++ interactions/commands/commandeGroupe/commandeGroupe.js
@@ -2,5 +2,5 @@ module.exports = {
 	name: "commande_groupe",
 	description: "Exemple de groupe de commandes.",
 	commandeGroupe: true,
-	category: "utils",//la categorie au quelle la commande sera ratache
+	category: "utils",//la categorie au quelle la commande sera ratache, laisser vide pour mettre sans categorie
 };
