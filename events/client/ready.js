2f962051df1484f9ae658ed5ac6bd9fa628058d7
Youritch <86657843+Youritch@users.noreply.github.com>
2023-11-11 19:42:01 +0100

    amélioration connection (erreurs + clean up)

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,5 @@
 const client = require("../../index").client;
-const clc = require("cli-color");
+const {blue} = require("cli-color");
 const {ActivityType, Status} = require("discord.js");
 
 client.once("ready", async () => {
@@ -7,7 +7,6 @@ client.once("ready", async () => {
 		activities: [{name: "status personnalisé", type: ActivityType.Watching}],
 		status: Status.Connecting,
 	});
-	
 	await client.application.commands.set(client.commands.map((cmd) => cmd));
-	console.log(clc.blue.bold.underline(`${client.user.tag} est connecté à discord.`));
+	console.log(blue.bold.underline(`${client.user.tag} est connecté à discord !`));
 });
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const {Client, GatewayIntentBits, Partials, Collection} = require("discord.js");
-const {greenBright} = require("cli-color");
+const {greenBright, redBright} = require("cli-color");
 const configFile = require("./config.json");
 const client = new Client({
 	intents: [
@@ -33,22 +33,37 @@ console.log(greenBright.bold.underline("Lancement du bot :"));
 client.config = configFile;
 
 //config bdd :
-if (configFile.bdd.enable) {
+if (configFile.bdd) {
 	require("./api/bdd.js");
 }
 
-//Création des collection discords pour les handlers :
+//Création des collections discords pour les handlers :
 client.commands = new Collection();
 client.buttons = new Collection();
 client.selects = new Collection();
 client.modals = new Collection();
 
+//export du client
 module.exports.client = client;
 
 //Chargement en mémoire des handlers :
 ["command", "event", "button", "select", "modal"].forEach(async (handler) => {
 	await require(`./handlers/preload/${handler}`)(client);
 });
-
+console.log(greenBright.bold.underline("Connection à discord…"));
 //Connection du bot :
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN).catch((reason) => {
+	console.log(redBright.bold("La connection à discord à échoué !"));
+	switch (reason.code) {
+		case "ENOTFOUND":
+			console.log(redBright("> Erreur de connection (vérifier votre connection à internet) !"));
+			break;
+		case "TokenInvalid":
+			console.log(redBright("> Token invalide (vérifier votre token sur https://discord.com/developers/applications et dans le fichier .ENV) !"));
+			break;
+		default:
+			if (process.env.DEBUG === "false") console.log(reason);
+			break;
+	}
+	if (process.env.DEBUG === "true") console.log(reason);
+});
