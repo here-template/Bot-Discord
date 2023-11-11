95a9b67179f65e2556164812030f5a9dd9864512
Youritch <86657843+Youritch@users.noreply.github.com>
2023-11-11 19:41:22 +0100

    amélioration api bdd

--- api/bdd.js
+++ api/bdd.js
@@ -1,33 +1,33 @@
 const {yellow, redBright, blackBright} = require("cli-color");
 const mysql = require("mysql");
-const config = require("../config.json");
 
-async function connectionPool(msg = true) {
-	let pool = mysql.createPool({
-		host: config.bdd.host, // Adresse de la base de donnée
-		port: config.bdd.port, // Port de la base de donnée
-		user: config.bdd.user, // Nom d'utilisateur de la base de donnée
-		password: config.bdd.password, // Mot de passe de la base de donnée
-		database: config.bdd.database, // Nom de la base de donnée
+async function connectionPool() {
+	const pool = mysql.createPool({
+		//toutes les données suivantes sont à rentrée dans le fichier .ENV oue en variable environment
+		host: process.env.HOST, // Adresse de la base de donnée
+		port: process.env.PORT, // Port de la base de donnée
+		user: process.env.LOGINUSER, // Nom d'utilisateur de la base de donnée
+		password: process.env.PASSWORD, // Mot de passe de la base de donnée
+		database: process.env.DATABASE, // Nom de la base de donnée
 		connectionLimit: 30, // Nombre de connections simultanés
 	});
 	// Envoie le retour d'un message en fonction de l'etat de la base de donnée
+	//ici, il faut configurer le test sur une table que vous avez
 	pool.query("SELECT test FROM test WHERE test = 'good_start'", function (err) { // Test de connection à la base de donnée (bien vérifier le wiki)
 		if (err) {
 			console.log(redBright("Erreur connection base de données.\n" + err));
 			console.log(redBright.bold(">> Shutdown ! <<"));
+			pool.destroy();//shutdown de la connection
 			const client = require("../index").client;
 			return client.destroy(); // Shutdown le bot si erreur de connection
 		}
-		if (msg) {
-			console.log(blackBright("Connection à la base de données réussite !"));
-		}
+		console.log(blackBright("Connection à la base de données réussite !"));
 	});
 	return pool;
 }
 
-// Avec sa plus besoin de test dans index.js
-const bddPool = connectionPool(true);
+//démarrage de la connection
+const bddPool = connectionPool();
 module.exports.pool = bddPool;
 
 /**
@@ -38,9 +38,9 @@ module.exports.query = async (re) => {
 		console.log("requête MySQL vide !");
 		return false;
 	}
-	let pool = await require("./bdd.js").pool;
+	const pool = await require("./bdd.js").pool;
 	try {
-		let result = new Promise((resolve, reject) => {
+		const result = new Promise((resolve, reject) => {
 			pool.query(re, function (err, result_query) {
 				if (err) return reject(err);
 				return resolve(result_query); //renvoi boolean, si @id à un compte ou pas
