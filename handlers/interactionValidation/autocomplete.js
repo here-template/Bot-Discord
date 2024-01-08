385ae1983046f6935807e8c94ee6867713213774
Youritch <86657843+Youritch@users.noreply.github.com>
2024-01-08 21:26:14 +0100

    amélioration autocomplete (oublie sub commande)

--- handlers/interactionValidation/autocomplete.js
+++ handlers/interactionValidation/autocomplete.js
@@ -1,5 +1,10 @@
 module.exports = (client, interaction) => {
-	const cmd = client.commands.get(interaction.commandName);
-	if (!cmd || !cmd.runAutocomplete) return [false];
-	return [true, cmd.runAutocomplete];
+    const cmd = client.commands.get(interaction.commandName);
+    if (cmd && cmd.isCommandeGroupe) {
+        const subcmd = cmd.options.find(c => c.name === interaction.options._subcommand)
+        if (!subcmd || !subcmd.runAutocomplete) return [false];
+        return [true, subcmd.runAutocomplete];
+    }
+    if (!cmd || !cmd.runAutocomplete) return [false];
+    return [true, cmd.runAutocomplete];
 };
