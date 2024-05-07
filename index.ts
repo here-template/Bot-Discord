a892fc703babae7a70f16b61651a3fa5e9fe082a
Cleboost <clement.balarot@gmail.com>
2024-05-07 17:04:33 +0200

    update handler import

--- index.ts
+++ index.ts
@@ -1,27 +1,30 @@
 // Creation Date: 07.05.2024
+// Template Version: 1.0
+// Created By: Cleboost & Youritch in My-Template Organization
 // Description: Main file of the bot
 
-// Import the CustomClient class
+
 import {CustomClient} from "./class/CustomClient";
-
-// Load the .env file
 import * as dotenv from 'dotenv'
-dotenv. config()
-
-// Import the Collection class from discord.js
 import {Collection} from "discord.js";
-import {greenBright, redBright} from "cli-color";
-
-const client: CustomClient = new CustomClient();
-
-console.log(greenBright.bold.underline("Lancement du bot :"));
-
+import {greenBright, magenta, redBright} from "cli-color";
+import Logger from "./class/logger/Logger";
 // Load the config file and set it to the client
 // @ts-ignore
 import configFile from "./config.json"
 import {Config} from "./interface/config";
+import Loader from "./class/logger/Loader";
+import {AsciiTree} from "oo-ascii-tree";
+
+dotenv. config()
+
+
+const client: CustomClient = new CustomClient();
+const logger: Logger = new Logger();
+
 const config: Config = configFile as Config
 client.config = config;
+client.logger = logger;
 
 client.commands = new Collection();
 client.buttons = new Collection();
@@ -35,25 +38,37 @@ export {client};
 // import
 
 // Start the handlers
-["command", "event", "button"].forEach(async (handler: string) => {
-	await import(`./handlers/preload/${handler}`).then((e) => e.default(client));
-});
+// @ts-ignore
 
-console.log(greenBright.bold.underline("Connecting to Discord..."));
 
-//Connection du bot :
-client.login(process.env.TOKEN).catch((reason) => {
-	console.log(redBright.bold("Discord connection failed !"));
-	switch (reason.code) {
-		case "ENOTFOUND":
-			console.log(redBright("> Error: No internet connection !"));
-			break;
-		case "TokenInvalid":
-			console.log(redBright("> Token invalide ! Please check your token in the .env file or reset it (https://discord.com/developers/applications)!"));
-			break;
-		default:
-			if (process.env.DEBUG === "false") console.log(reason);
-			break;
+(async () => {
+	const loader: Loader = new Loader();
+	loader.start()
+	const tree = new AsciiTree("bot");
+	for (const handler of ["command", "event", "button"]) {
+		loader.setText("Loading of " + handler);
+		const module = await import(`./handlers/preload/${handler}`);
+		await module.default(client, loader, tree);
 	}
-	if (process.env.DEBUG === "true") console.log(reason);
-});
+	loader.stop();
+	tree.printTree();
+})().then(() => {
+	console.log(magenta.bold.underline("All handlers loaded !"));
+	process.stdout.write(greenBright.bold.underline("Connecting to Discord..."));
+	client.login(process.env.TOKEN).catch((reason) => {
+		process.stdout.write('\x1B[0G\x1B[2K');
+		console.log(redBright.bold("Discord connection failed !"));
+		switch (reason.code) {
+			case "ENOTFOUND":
+				console.log(redBright("> Error: No internet connection !"));
+				break;
+			case "TokenInvalid":
+				console.log(redBright("> Token invalide ! Please check your token in the .env file or reset it (https://discord.com/developers/applications)!"));
+				break;
+			default:
+				if (process.env.DEBUG === "false") console.log(reason);
+				break;
+		}
+		if (process.env.DEBUG === "true") console.log(reason);
+	});
+})
