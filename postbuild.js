0ae3c7ad570d34f29ec5b4c187ddb052d201beed
Cleboost <clement.balarot@gmail.com>
2024-09-24 20:33:36 +0200

    feat: postbuild script added

---
+++ postbuild.js
@@ -0,0 +1,26 @@
+import fs from 'node:fs';
+
+const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+packageJson.main = 'index.js';
+packageJson.scripts = {
+    'start': 'node .',
+    'db': 'prisma db push'
+};
+
+fs.writeFileSync('./dist/package.json', JSON.stringify(packageJson, null, 2));
+
+fs.copyFileSync('prisma/', 'dist/prisma/');
+fs.copyFileSync('.env', 'dist/.env');
+fs.copyFileSync('config/', 'dist/config/');
+
+const needFolder = ['interactions/context/message', 'interactions/context/user', 'interactions/buttons', 'interactions/commands', 'interactions/modals', 'interactions/selects'];
+needFolder.forEach((folder) => {
+    if (!fs.existsSync(`dist/${folder}`)) fs.mkdirSync(`dist/${folder}`, {recursive: true});
+});
+
+
+/*
+    This script will be executed after the build command.
+    It will copy the prisma folder, the .env file, the config folder and create the needed folders for the interactions
+    It will also modify the package.json file to have the right main file and scripts
+*/
