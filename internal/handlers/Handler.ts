ea3f6dba8654f3cf39fc07c8166bb2c0fc9ce9ef
Cleboost <clement.balarot@gmail.com>
2025-01-04 01:34:50 +0100

    feat: ajouter la classe Handler pour gérer les collections et les événements

---
+++ internal/handlers/Handler.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2025 Cleboost & Youritch from Here-Template
+ * External contributor can be found on the GitHub
+ * Licence: Not curently licensed
+ */
+
+import { Collection } from "discord.js";
+import { BotClient } from "../class/BotClient";
+
+export class Handler {
+	protected collection: Collection<unknown, unknown> = new Collection();
+	client: BotClient;
+
+	constructor(client: BotClient) {
+		this.client = client;
+	}
+
+	async load(): Promise<void> {}
+	async event(): Promise<void> {}
+	getCollections() {
+		return this.collection;
+	}
+}
