6ff1d11b66d208ee9aa9027a55c58b7dd6dfa9ca
Cleboost <clement.balarot@gmail.com>
2025-01-04 01:28:02 +0100

    feat: supprimer les interfaces obsolètes pour simplifier la structure du projet

--- interface/modal.d.ts
+++
@@ -1,7 +0,0 @@
-import {ModalSubmitInteraction} from "discord.js";
-import {CustomClient} from "../class/CustomClient";
-
-interface Modal {
-    customID: string;
-    run: (client: CustomClient, interaction: ModalSubmitInteraction) => void;
-}
