de13d679b6c3b1bbd5a5a9fd3175373605f68572
Cleboost <clement.balarot@gmail.com>
2025-01-08 20:49:41 +0100

    chore: remove deprecated configuration and interface files

--- interface/button.d.ts
+++
@@ -1,8 +0,0 @@
-import {ButtonInteraction} from "discord.js";
-import {CustomClient} from "../class/CustomClient";
-
-interface Button {
-    customID: string;
-    admin?: boolean | undefined;
-    run: (client: CustomClient, interaction: ButtonInteraction) => void;
-}
