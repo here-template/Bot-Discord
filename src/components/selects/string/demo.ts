80d994705bc6a85e5437ff37d6b2acc19046d120
Cleboost <clement.balarot@gmail.com>
2025-12-21 21:20:52 +0100

    format

--- src/components/selects/string/demo.ts
+++ src/components/selects/string/demo.ts
@@ -1,18 +1,18 @@
 import { StringSelectMenu } from "@djs-core/runtime";
 
 export default new StringSelectMenu()
-    .addOptions([
-        {
-            label: "Option 1",
-            value: "option1",
-        },
-        {
-            label: "Option 2",
-            value: "option2",
-        },
-    ])
-    .run(async (interaction) => {
-        return interaction.reply({
-            content: `You selected ${interaction.values[0]}`,
-        });
-    });
+	.addOptions([
+		{
+			label: "Option 1",
+			value: "option1",
+		},
+		{
+			label: "Option 2",
+			value: "option2",
+		},
+	])
+	.run(async (interaction) => {
+		return interaction.reply({
+			content: `You selected ${interaction.values[0]}`,
+		});
+	});
--- src/interactions/commands/handler/select.ts
+++ src/interactions/commands/handler/select.ts
@@ -3,10 +3,12 @@ import { Command } from "@djs-core/runtime";
 import { ActionRowBuilder, type StringSelectMenuBuilder } from "discord.js";
 
 export default new Command()
-    .setDescription("Handle a select")
-    .run(async (interaction) => {
-        return interaction.reply({
-            content: "Select handled",
-            components: [new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(demo)],
-        });
-    });
+	.setDescription("Handle a select")
+	.run(async (interaction) => {
+		return interaction.reply({
+			content: "Select handled",
+			components: [
+				new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(demo),
+			],
+		});
+	});
